7c5d098ee2e6bf04c5a8fe0a734cec5fd8be417b
edanherrera <edanherreraga@ittepic.edu.mx>
2022-05-03 00:10:14 -0600

    Clase 28/Abril

--- app/src/main/java/mx/tecnm/tepic/ladm_u3_ejercicio4_firestore/MainActivity.kt
+++ app/src/main/java/mx/tecnm/tepic/ladm_u3_ejercicio4_firestore/MainActivity.kt
@@ -2,6 +2,7 @@ package mx.tecnm.tepic.ladm_u3_ejercicio4_firestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.google.firebase.firestore.FirebaseFirestore
@@ -14,6 +15,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //EVENTO (SE DISPARA SOLO)
+        FirebaseFirestore.getInstance()
+            .collection("persona")
+            .addSnapshotListener { query, error ->
+                if (error!=null){
+                    //SI HUBO ERROR!
+                    AlertDialog.Builder(this)
+                        .setMessage(error.message)
+                        .show()
+                    return@addSnapshotListener
+                }
+
+                val arreglo = ArrayList<String>()
+                for (documento in query!!){
+                    var cadena = "nombre: ${documento.getString("nombre")}\n" +
+                            "Edad:  ${documento.getLong("edad")}"
+                    arreglo.add(cadena)
+                }
+
+                binding.lista.adapter= ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, arreglo)
+            }
 
         binding.insertar.setOnClickListener{
 
